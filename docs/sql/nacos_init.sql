52fdd3504195eb45241cd456089886a843b6d93f
chenghekai1 <chenghekai1@jd.com>
2024-02-28 15:55:39 +0800

    nacos_init.sql修正

--- docs/sql/nacos_init.sql
+++ docs/sql/nacos_init.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `config_info`
 --
-
+CREATE DATABASE `nacos`; /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */
+use nacos;
 DROP TABLE IF EXISTS `config_info`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
